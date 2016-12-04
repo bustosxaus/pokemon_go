e0030dc651642bb1ff25ba74f7408ae68fa43d06
Gonzalo Bustos <gonzalo.bustos@stat.duke.edu>
2016-12-04 18:00:35 -0500

    final plots

--- initial_map_113016_v3.R
+++ initial_map_113016_v3.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(ggmap)
 library(lubridate)
 library(stringr)
+library(gridExtra)
 
 pokemon = fread('300k.csv')
 
@@ -37,6 +38,13 @@ sarita$hour_interval = cut(sarita$poketime,
 #get countries
 sarita$countries = coords2country(data.frame(sarita$longitude,sarita$latitude))
 
+#grouped terrain type
+sarita$terrain_grouped = "water"
+sarita$terrain_grouped[sarita$terrainType %in% c(1,2,4,5)] = "forest"
+sarita$terrain_grouped[sarita$terrainType %in% c(7,8,9,16)] = "savanna"
+sarita$terrain_grouped[sarita$terrainType %in% c(10,11,12)] = "grassland"
+sarita$terrain_grouped[sarita$terrainType %in% c(13)] = "urban"
+
 #filter USA
 usa = sarita %>% filter(countries == 'United States of America')
 
@@ -45,15 +53,6 @@ map_usa = get_map(location = 'USA', zoom = 4, color = 'bw', source = 'google')
 ggmap(map_usa) + 
         geom_point(data = usa, aes(x = longitude, y = latitude, 
                                   color = closeToWater))
-#heat map
-usa_map = ggmap(map_usa, base_layer = ggplot(aes(x = longitude, y = latitude), 
-                                               data = sarita))
-usa_map +
-        stat_density2d(aes(x = longitude, y = latitude, fill = ..level.., 
-                           alpha = ..level..), bins = 70, geom = 'polygon', 
-                       data = usa) +
-        scale_fill_gradient(low = 'gold', high = 'red', guide = F) +
-        guides(alpha = F)
 
 #filter Chile
 chile = sarita %>% filter(countries == 'Chile')
@@ -93,12 +92,49 @@ reorder_size = function(x) {
         factor(x, levels = names(sort(table(x), decreasing = T)))
 }
 
-ggplot(data = sarita, aes(reorder_size(first_type))) + 
+#eda plots
+water_plot = ggplot(data = sarita, aes(reorder_size(first_type))) + 
         geom_bar(aes(fill = closeToWater), position = 'fill') +
+        xlab('pokemon type') +
+        ylab('') +
+        labs(fill = 'near water') +
+        theme(legend.position = 'top') + 
+        theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+terrain_plot = ggplot(data = sarita, aes(reorder_size(first_type))) + 
+        geom_bar(aes(fill = terrain_grouped), position = 'fill') +
+        xlab('pokemon type') +
+        ylab('') +
+        labs(fill = 'terrain') +
+        theme(legend.position = 'top') + 
+        theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+grid.arrange(terrain_plot, water_plot, ncol = 2)
+
+#heat map
+usa_map = ggmap(map_usa, base_layer = ggplot(aes(x = longitude, y = latitude), 
+                                             data = sarita))
+usa_map +
+        stat_density2d(aes(x = longitude, y = latitude, fill = ..level.., 
+                           alpha = ..level..), bins = 30, geom = 'polygon', 
+                       data = usa) +
+        scale_fill_gradient(low = 'gold', high = 'red', guide = F) +
+        guides(alpha = F)
+
+#pokemon type frequency
+freq_type = ggplot(data = sarita, aes(reorder_size(first_type))) + 
+        geom_bar() +
         xlab('pokemon type')
 
-ggplot(data = sarita, aes(reorder_size(first_type))) + 
-        geom_bar()
+#playing with hour interval plots
+ggplot(data = sarita, aes(hour_interval)) + 
+        geom_bar() +
+        xlab('2-hour interval') +
+        facet_wrap(~ first_type)
 
-ggplot(data = sarita, aes(x = first_type, y = total_stats)) + 
-        geom_boxplot()
+hour_counts = sarita %>%
+        group_by(hour_interval, first_type) %>%
+        summarize(count = n())
+
+ggplot(hour_counts, aes(x = hour_interval, y = count, group = first_type)) +
+        geom_line()
